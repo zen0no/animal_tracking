c94cbc07559edbe2bc2c12656929fe0eb62ef725
dimkablin <dmitryellison@gmail.com>
2024-07-11 13:36:48 +0300

    features/added usage example

--- src/env/env.py
+++ src/env/env.py
@@ -3,4 +3,4 @@ import os
 DATA_FOLDER = os.getenv("DATA_FOLDER", "data") # temp files folder's name
 SPICES_FILE = os.getenv("SPICES_FILE", "src/env/spices.json") # spices file
 BATCH_SIZE = int(os.getenv("BATCH_SIZE", 32))
-YOLO_PATH = os.getenv("YOLO_PATH", "weights/yolo.pt")
+YOLO_PATH = os.getenv("YOLO_PATH", "../weights/yolo.pt")
